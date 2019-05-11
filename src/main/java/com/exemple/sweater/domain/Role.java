da2ccffa1ec37294071c9c2b6942dfbd7d63a0d0
JUST TESTER <artyr.savytskyy@gmail.com>
2019-05-12 00:41:44 +0300

    Add registration functional

---
+++ src/main/java/com/exemple/sweater/domain/Role.java
@@ -0,0 +1,5 @@
+package com.exemple.sweater.domain;
+
+public enum Role {
+    USER;
+}
---
+++ src/main/java/com/exemple/sweater/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.exemple.sweater.repos;
+
+import com.exemple.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long>{
+    User findByUsername(String username);
+}
